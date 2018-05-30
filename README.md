ce43e77f5464d77dee8bea2ad97ce938a73755f5
john <nergilz@yandex.ru>
2018-05-30 10:20:09 +0200

    first version

--- README.md
+++ README.md
@@ -1,6 +1,61 @@
-# Sites Monitoring Utility
+## Sites Monitoring Utility
 
-[TODO. There will be project description]
+---
+The script to check the status of our sites
+
+### Discription
+
++ Test: the server responds to the request with the status of HTTP 200
++ Test: the domain name paid for a month or more
++ You need a library [requests](http://docs.python-requests.org/en/master/user/quickstart/)
++ and library [python-whois](https://pypi.org/project/python-whois/)
++ It is recommended to use [virtualenv](https://docs.python.org/3/library/venv.html) 
++ File name with urls for check: "urls.txt" 
+
+
+### How to install requests and pyton-whois
+```bash
+pip install -r requirements.txt
+```
+
+### Start script example
+
+```bash
+python3 check_sites_health.py
+```
+
+### Start script with your file for check
+```bash
+python3 check_sites_health.py --path "path to file"
+``` 
+
+### Example result
+```bash
+https://github.com, status: True, domain payment > month: True, date to: 2020 10 09
+https://devman.org, status: True, domain payment > month: True, date to: 2018 08 28
+https://python-scripts.com, status: True, domain payment > month: True, date to: 2018 09 06
+https://toster.ru, status: True, domain payment > month: True, date to: 2018 08 08
+https://google.com, status: True, domain payment > month: True, date to: 2020 09 14
+https://habr.com, status: True, domain payment > month: True, date to: 2019 03 11
+https://pypi.org; status: True; domain_payment_>_month: True; date to: 2018-07-24
+```
+
+### Example result with errors
+```bash
+https://toster.ru, status: True, domain payment > month: True, date to: 2018 08 08
+ ERROR WHOIS responce: https://pypi.org; error: [Errno 104] Connection reset by peer
+https://google.com, status: True, domain payment > month: True, date to: 2020 09 14
+https://habr.com, status: True, domain payment > month: True, date to: 2019 03 11
+ ERROR https://pypi.org; socket timeout: timed out
+```
+
+### Requirements
+
+```bash
+Python ver 3.5 (or higher)
+```
+
+---
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,107 @@
+import whois
+import argparse
+import datetime
+import requests
+import sys
+import calendar
+import socket
+
+
+def get_arguments():
+
+    parser = argparse.ArgumentParser(
+        description='Path to domain names file'
+        )
+    parser.add_argument(
+        '--path',
+        required=False,
+        default='urls.txt',
+        help='Optional parameter: "urls.txt"'
+        )
+    return parser.parse_args()
+
+
 def load_urls4check(path):
-    pass
+
+    with open(path, 'r') as file_handler:
+        list_urls = file_handler.read().split('\n')
+
+    return [url for url in list_urls if url is not '']
+
 
 def is_server_respond_with_200(url):
-    pass
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return True
+    else:
+        return False
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+
+    response = whois.whois(domain_name)
+    expiration_date = response["expiration_date"]
+
+    if isinstance(expiration_date, list):
+        return expiration_date[0]
+    else:
+        return expiration_date
+
+
+def check_payment_date(expiration_date):
+
+    date_now = datetime.datetime.now()
+    days_in_month = calendar.monthrange(date_now.year, date_now.month)[1]
+    check_date = date_now + datetime.timedelta(days=days_in_month)
+
+    if check_date < expiration_date:
+        return True
+    else:
+        return False
+
+
+def pprint_sites_health(url, status, check_date, expiration_date):
+
+    print('{}; status: {}; domain_payment_>_month: {}; date to: {}'.format(
+        url,
+        status,
+        check_date,
+        expiration_date.strftime('%Y-%m-%d')
+        )
+    )
+
+
+def check_urls(list_urls4check):
+
+    for url in list_urls4check:
+
+        try:
+            status = is_server_respond_with_200(url)
+            expiration_date = get_domain_expiration_date(url)
+            check_date = check_payment_date(expiration_date)
+            pprint_sites_health(
+                url,
+                status,
+                check_date,
+                expiration_date
+                )
+
+        except ConnectionResetError as err:
+            print(' ERROR WHOIS response: {}; error: {}'.format(url, err))
+
+        except socket.error as err:
+            print(' ERROR {}; socket: {}'.format(url, err))
+
 
 if __name__ == '__main__':
-    pass
+    arguments = get_arguments()
+
+    try:
+        list_urls4check = load_urls4check(arguments.path)
+
+    except FileNotFoundError as err:
+        sys.exit(' ERROR: {}'.format(err))
+
+    check_urls(list_urls4check)
